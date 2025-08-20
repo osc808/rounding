4a26f2477f6e5775f0d66653195cde6c430669c7
osc808 <oscars.23111@obhs.school.nz>
2025-08-21 09:07:39 +1200

    Rounding methods

---
+++ rounding_ways.py
@@ -0,0 +1,11 @@
+
+
+total = (10.2823 + 7.6991) / 2.66
+print(f'The total is: {total:2f}',round(total, 2))    #Displaying as a float a number
+
+
+
+#string formatting method... slicing...
+#word = 'Butter'
+#word[2:5]
+#['a','b','c','d']   #[0:3]
